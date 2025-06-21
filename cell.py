c60b4876524b5c26b2695b0f7c4571ff0ea9a3db
pranavpande01 <pranav4740@gmail.com>
2025-06-21 19:19:23 +0530

    cleaner code

--- cell.py
+++ cell.py
@@ -19,9 +19,6 @@ class Cell:
 
         
         self.params=params
-        self.ws=params[0]
-        self.wi=params[1]
-        self.b=params[2]
 
         self.short = short_array.astype(float)
         self.input = input_array.astype(float)
@@ -33,19 +30,21 @@ class Cell:
 
 
     def forward(self):
-        x=self.short*self.ws[0]+self.input*self.wi[0]+self.b[0]
-        x=sigmoid(x)
-        self.long=self.long*x
 
-        x=sigmoid(self.short*self.ws[1]+self.input*self.wi[1]+self.b[1])*tanh(self.short*self.ws[2]+self.input*self.wi[2]+self.b[2])
-        self.long=self.long+x
+        ws,wi,b=self.params[0],self.params[1],self.params[2]
 
-        x=sigmoid(self.short*self.ws[3]+self.input*self.wi[3]+self.b[3])
-        x=tanh(self.long)*x
-        self.short=x
-        self.context_vector=self.long,self.short
-       
-        self.context_vector
+        x = sigmoid(self.short * ws[0] + self.input * wi[0] + b[0])
+        self.long = self.long * x
+
+        x = sigmoid(self.short * ws[1] + self.input * wi[1] + b[1]) * tanh(self.short * ws[2] + self.input * wi[2] + b[2])
+        self.long = self.long + x
+
+        x = sigmoid(self.short * ws[3] + self.input * wi[3] + b[3])
+        self.short = tanh(self.long) * x
+
+        self.context_vector = (self.long, self.short)
+
+        return self.context_vector
 
 
 
@@ -54,6 +53,6 @@ params=[np.array([2.7,2,1.41,4.38]),np.array([1.62,1.65,0.94,-0.19]),np.array([1
 short=np.array([1,2])
 input=np.array([1,2])
 long=np.array([2,2])
-cell=Cell(short_array=short,input_array=input,long_array=long)
+cell=Cell(short_array=short,input_array=input,long_array=long,params=params)
 cell.forward()
 print(cell.context_vector)
